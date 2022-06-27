080a551eae0c8d810c0149e497a3e027e23f74b0
ihsanul arifin <108244035+ihsanularifin022103@users.noreply.github.com>
2022-06-27 19:42:31 +0700

    Add files via upload

---
+++ DAYS-1/Exercise-level-1.js
@@ -0,0 +1,51 @@
+
+
+// Write a single line comment which says, comments can make code readable
+// comments can make code readable
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* comments can make code readable, 
+easy to reuse and informative */
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+var num = 10
+var str = 'this string'
+var bool = true
+var undif = undefined
+var nul = null
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+typeof(num) // integer
+typeof(str) // string
+typeof(bool) // boolean
+typeof(undif) // undefined
+typeof(nul) // null
+
+// Declare four variables without assigning values
+var noValue
+
+// Declare four variables with assigned values
+var withValue = 10
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+var firstName = 'ihsanul'
+var lastName = 'arifin'
+var statusMarried = false
+var country = 'indonesia'
+var age = 18
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+var identity = 'My name is '+firstName+' '+lastName+' from '+country+' and my age is '+age
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+// I am 25 years old.
+// You are 30 years old.
+var myAge = 25
+var yourAge = 30
+var myIdentity = 'I am '+myAge+' years old.'
+var yourIdentity = 'You are '+yourAge+' years old.'
+console.log(myIdentity)
+console.log(yourAge)
